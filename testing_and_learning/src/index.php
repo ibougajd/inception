66648fbdaa4ee8cb60e4bb8865d287b0d8c38719
ibougajd <ilyassbougajdi1337@gamil.com>
2025-12-12 00:12:18 +0100

    05

---
+++ testing_and_learning/src/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Hello from PHP-FPM!</h1>";
+echo "<p>This page was served by the PHP-FPM container (port 9000) and accessed via the Nginx container (port 80).</p>";
+echo "<p>PHP Version: " . phpversion() . "</p>";
+?>
